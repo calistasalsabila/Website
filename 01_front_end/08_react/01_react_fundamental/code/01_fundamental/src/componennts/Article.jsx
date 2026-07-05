16b4fc6a576ee10df76325241ef8aa85c39053be
calista salsabila <calistasalsabila313@gmail.com>
2026-07-05 10:02:51 +0700

    with return

--- 01_front_end/08_react/01_react_fundamental/code/01_fundamental/src/componennts/Article.jsx
+++ 01_front_end/08_react/01_react_fundamental/code/01_fundamental/src/componennts/Article.jsx
@@ -2,10 +2,14 @@ function Article(){
     const name = "Caltherion";
     const title = ["orv", "kp", "tog"];
 
+    // title.map(item => item);
+
     return(
         <>
             <div>{name}</div>
-            <div>{title}</div>
+            <div>{title.map((title => {
+                return <div>{title}</div>
+            }))}</div>
         </>
     )
 }
