21018e66a2753ff9a61b38cca6a248fbf4b12c10
calista salsabila <calistasalsabila313@gmail.com>
2026-07-05 11:56:09 +0700

    fitur filter data dan pengenalan useState

--- 01_front_end/08_react/01_react_fundamental/code/01_fundamental/src/pages/Index.jsx
+++ 01_front_end/08_react/01_react_fundamental/code/01_fundamental/src/pages/Index.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import posts from "../posts.json"
 import Article from "../componennts/Article"
 
 
 function Homepage(){
+
+    const [search, setSearch] = useState("");
+    const changeSearch = (event) => {
+        setSearch(event.target.value);
+    }
     return(
         <>
             <h1>Simple Blog</h1>
+            <div>Cari Article : <input onChange={changeSearch}></input></div>
+            <small>ditemukan 0 data dengan pencarian kata {search}</small>
             {
-                posts.map(({title, tags, aka})=>(
+                posts.map(({title, tags, aka}, index)=>(
                     // <Article title={title} tags={tags} aka={aka} />
-                    <Article {...{title, tags, aka}} />
+                    <Article {...{title, tags, aka}} key={index} />
                 ))
             }
         </>
