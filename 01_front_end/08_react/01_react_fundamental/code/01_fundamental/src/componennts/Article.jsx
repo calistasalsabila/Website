9b192c7419205f4235ac1406509ada18166a85b3
calista salsabila <calistasalsabila313@gmail.com>
2026-08-18 13:23:11 +0700

    feat: add conditional rendering dan ternary operator

--- 01_front_end/08_react/01_react_fundamental/code/01_fundamental/src/componennts/Article.jsx
+++ 01_front_end/08_react/01_react_fundamental/code/01_fundamental/src/componennts/Article.jsx
@@ -3,7 +3,11 @@ function Article(props){
         <>
             <h3>{props.title}</h3>
 
-            <small>aka: {props.aka}, tags: {props.tags.join(", ")}</small>
+            <small>aka: {props.aka}, tags: {props.tags.join(", ")} {" "}
+                {props.isNew ? 'baru' : 'lama'}{" "}
+                {/* conditional rendering */}
+                {props.isNew && 'baruuu'}
+            </small>
         </>
     )
 }
