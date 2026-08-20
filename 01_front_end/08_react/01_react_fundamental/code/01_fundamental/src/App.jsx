c2d8715e31cd755a6e9bcd42b6ecf80c4f8e609f
calista salsabila <calistasalsabila313@gmail.com>
2026-08-20 16:35:50 +0700

    feat: add react router

--- 01_front_end/08_react/01_react_fundamental/code/01_fundamental/src/App.jsx
+++ 01_front_end/08_react/01_react_fundamental/code/01_fundamental/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Home from './pages/Index'
 import { GlobalContext } from './context'
+import { RouterProvider } from 'react-router-dom'
+import {router} from './routers'
 
 function App() {
 
@@ -11,8 +13,10 @@ function App() {
     <>
       <div className="ticks">
         <GlobalContext.Provider value={user}>
-          <Home  /> 
+          {/* <Home  />  hilang karena dah di ganti sama router */}
+          <RouterProvider router={router} />
         </GlobalContext.Provider>
+
 
       </div>
     </>
